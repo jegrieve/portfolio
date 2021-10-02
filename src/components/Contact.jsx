68e814bce75f2688cbd98a108351fff3a6cb0419
James Grieve <pocojames@hotmail.com>
2021-10-02 00:58:37 -0700

    add basic contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import "../styles/Contact.css";
 const Contact = () => {
 
     return (
-        <div>
-            Contact
+        <div className = "contact-container container">
+            <div className = "row">
+                <div className = "col-12">
+                    <div className = "contact-title">Contact</div>
+                </div>
+            </div>
+            <hr />
+            <div className = "row">
+                <div className = "col-12">
+                    <div className = "contact-body">To get in contact, please send me an email at <a className = "contact-email" href="mailto: jegrieve7@gmail.com" target="_blank">jegrieve7@gmail.com</a></div>
+                </div>
+            </div>
         </div>
     )
 };
